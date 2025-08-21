c5dfb469343eb5b29207b0d1942248f8672505f5
lintao <e1406511@.u.nus.edu>
2025-08-22 01:55:28 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/src/Clover.java
@@ -0,0 +1,13 @@
+public class Clover {
+    public static void main(String[] args) {
+
+        String line = "______________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello!, I'm Clover");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+    }
+}
