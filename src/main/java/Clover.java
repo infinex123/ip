ab92d740e3a297cc170fcc1215976ecdedbaac7e
lintao <e1406511@.u.nus.edu>
2025-08-22 23:30:50 +0800

    feat(level-0): implement echo

--- src/main/java/Clover.java
+++ src/main/java/Clover.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Clover {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
 
         String line = "______________________________________________________";
@@ -11,9 +12,15 @@ public class Clover {
         System.out.println("Hello!, I'm Clover");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
 
-
+        while (in.hasNextLine()) {
+            String input = in.nextLine().trim();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("goodbye! hope to see you again!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
