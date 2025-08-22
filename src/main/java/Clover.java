bfa2cb44d817e3ba6eba1a531392438ec354d3d7
lintao <e1406511@.u.nus.edu>
2025-08-23 00:21:54 +0800

    feat(level-2): implement Add, List

--- src/main/java/Clover.java
+++ src/main/java/Clover.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Clover {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] items = new String[100];
+        int size = 0;
 
 
         String line = "______________________________________________________";
@@ -18,6 +20,15 @@ public class Clover {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("goodbye! hope to see you again!");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < size; i++) {
+                    System.out.println(" " + (i + 1) + "." + items[i]);
+                }
+            } else if (!input.isEmpty()) {
+                if (size < items.length) {
+                    items[size++] = input;
+                    System.out.println("added: " + input);
+                }
             } else {
                 System.out.println(input);
             }
