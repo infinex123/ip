72ba80149bf835d63194483b96efbcb8b6c07514
lintao <e1406511@.u.nus.edu>
2025-08-23 14:17:21 +0800

    feat(level-3): implement Mark as Done

--- src/main/java/Clover.java
+++ src/main/java/Clover.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Clover {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int size = 0;
 
 
@@ -16,22 +16,43 @@ public class Clover {
         System.out.println(line);
 
         while (in.hasNextLine()) {
-            String input = in.nextLine().trim();
+            String input =  in.nextLine().trim();
+            Task task = new Task(input);
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println("goodbye! hope to see you again!");
+                System.out.println("Bye, hope to see you again soon!!");
                 break;
             } else if (input.equalsIgnoreCase("list")) {
-                for (int i = 0; i < size; i++) {
+                System.out.println("Here are the tasks in your list");
+                for (int i = 0; i < size;i++) {
                     System.out.println(" " + (i + 1) + "." + items[i]);
                 }
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                markOrUnmark(items, input, true);
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                markOrUnmark(items, input, false);
             } else if (!input.isEmpty()) {
                 if (size < items.length) {
-                    items[size++] = input;
+                    items[size++] = task;
                     System.out.println("added: " + input);
+                } else {
+                    System.out.println("full!!");
                 }
-            } else {
-                System.out.println(input);
             }
+        }
+    }
+
+    private static void markOrUnmark(Task[] tasks, String input, boolean mark) {
+        String[] parts = input.split("\\s+", 2);
+        int index = Integer.parseInt(parts[1]) - 1;
+        Task task = tasks[index];
+        if (mark) {
+            task.markDone();
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("   " + task);
+        } else {
+            task.markUndone();
+            System.out.println(" OK, I've marked this task as not done yet:");
+            System.out.println("   " + task);
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon(){
+        if(isDone) {
+            return "X";
+
+        } else {
+            return "";
+        }
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
